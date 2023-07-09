57fa709ff81e6304f94c1d632be435c8b5088da0
t-hata-job <t-hata-job@users.noreply.github.com>
2023-07-09 11:42:13 +0000

    deploy: 12a30214ea64cd3df89399af15241227615a09c0

--- _nuxt/56a6664.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{470:function(n,e,l){"use strict";l.r(e);var t={components:{Clanedar:l(300).default},name:"IndexPage"},c=l(79),component=Object(c.a)(t,(function(){return(0,this._self._c)("clanedar")}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/9140d00.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{467:function(n,e,l){"use strict";l.r(e);var t={components:{Clanedar:l(300).default},name:"IndexPage"},c=l(83),component=Object(c.a)(t,(function(){return(0,this._self._c)("clanedar")}),[],!1,null,null,null);e.default=component.exports}}]);
