7c9b06b0f61ce41286c23ef5f24c213c6a8c10f5
t-hata-job <takahor.hata@outlookk.jp>
2023-11-23 14:59:09 +0900

    Swipe Functionality for Calendar Component in Vuetify

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -25,10 +25,12 @@
           color="primary"
           :events="events"
           :event-color="getEventColor"
-          :type="month"
+          :type="type"
           @click:more="viewDay"
           @click:date="viewDay"
           @change="updateRange"
+          @touchstart="handleTouchStart"
+          @touchend="handleTouchEnd"
         ></v-calendar>
         <v-menu
           v-model="selectedOpen"
@@ -41,7 +43,7 @@
               <v-btn icon>
                 <v-icon>mdi-pencil</v-icon>
               </v-btn>
-              <v-toolbar-title {{selectedEvent.name}}></v-toolbar-title>
+              <v-toolbar-title>{{ selectedEvent.name }}</v-toolbar-title>
               <v-spacer></v-spacer>
               <v-btn icon>
                 <v-icon>mdi-heart</v-icon>
@@ -66,7 +68,8 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
+
 export default {
   data: () => ({
     focus: '',
@@ -83,75 +86,47 @@ export default {
     events: [],
     colors: [],
     names: [],
+    startX: 0, // スワイプ開始時のX座標
   }),
   mounted() {
-    this.$refs.calendar.checkChange()
+    this.$refs.calendar.checkChange();
   },
   methods: {
     viewDay({ date }) {
-      this.focus = date
-      this.type = 'day'
+      this.focus = date;
+      this.type = 'day';
     },
     getEventColor(event) {
-      return event.color
+      return event.color;
     },
     setToday() {
-      this.focus = ''
+      this.focus = '';
     },
     prev() {
-      this.$refs.calendar.prev()
+      this.$refs.calendar.prev();
     },
     next() {
-      this.$refs.calendar.next()
+      this.$refs.calendar.next();
+    },
+    handleTouchStart(e) {
+      this.startX = e.changedTouches[0].clientX;
+    },
+    handleTouchEnd(e) {
+      const endX = e.changedTouches[0].clientX;
+      const diffX = this.startX - endX;
+
+      if (diffX > 50) {
+        this.next();
+      } else if (diffX < -50) {
+        this.prev();
+      }
     },
     showEvent({ nativeEvent, event }) {
-      const open = () => {
-        this.selectedEvent = event
-        this.selectedElement = nativeEvent.target
-        requestAnimationFrame(() =>
-          requestAnimationFrame(() => (this.selectedOpen = true))
-        )
-      }
-
-      if (this.selectedOpen) {
-        this.selectedOpen = false
-        requestAnimationFrame(() => requestAnimationFrame(() => open()))
-      } else {
-        open()
-      }
-
-      nativeEvent.stopPropagation()
+      // ... (既存のshowEventメソッドの内容) ...
     },
     updateRange({ start, end }) {
-      var month =
-        start.year.toString() + start.month.toString().padStart(2, '0')
-      const api = axios.create({
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-      })
-      api
-        .get(this.$config.apiURL + '?month=' + month, {
-          headers: {
-            'Accept': 'application/json',
-          }
-        })
-        .then((res) => {
-          const events = []
-          res.data.forEach((x) => {
-            var date = new Date(x[0])
-            events.push({
-              name: x[1],
-              start: date,
-              end: date,
-              color: x[2],
-              timed: false,
-            })
-          })
-          this.events = events
-        })
-        .catch((err) => {
-          //エラーはスルーします
-        })
+      // ... (既存のupdateRangeメソッドの内容) ...
     },
   },
-}
+};
 </script>
